20a8e669228734cba8195705269007af18edb82c
Christian Liebel <christian.liebel@thinktecture.com>
2020-11-13 09:34:27 +0100

    Todo controller boilerplate

---
+++ src/todo/todo.controller.ts
@@ -0,0 +1,37 @@
+import { Todo } from './../todo';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Post, Put } from '@nestjs/common';
+
+// This should be "todos", not "todo"
+@Controller('todos')
+export class TodoController {
+
+    // GET /todos
+    @Get()
+    getAll(): Todo[] {
+    }
+
+    // GET /todos/:id
+    @Get(':id')
+    get(@Param('id') id: string): Todo {
+        // throw new NotFoundException();
+    }
+
+    // POST /todos
+    @Post()
+    @HttpCode(HttpStatus.CREATED)
+    add(@Body() todo: Todo): Todo {
+    }
+
+    // PUT /todos/:id
+    @Put(':id')
+    @HttpCode(HttpStatus.NO_CONTENT)
+    update(@Param('id') id: string, @Body() todo: Todo): void {
+    }
+
+    // DELETE /todos/:id
+    @Delete(':id')
+    @HttpCode(HttpStatus.NO_CONTENT)
+    delete(@Param('id') id: string): void {
+    }
+
+}
